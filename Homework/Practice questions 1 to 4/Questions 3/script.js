eb7545edad1e855f2136da8e9b2eb8ddd0b491db
Brandon Shea <brandon.shea1994@icloud.com>
2024-05-31 20:22:57 -0230

    5 to 7

--- Homework/Practice questions 1 to 4/Questions 3/script.js
+++ Homework/Practice questions 1 to 4/Questions 3/script.js
@@ -1,3 +1,7 @@
+// Practice questions 3
+// Brandon Shea
+// May 27th - 31st
+
 // 1)
 function reverseNum(num) {
   return parseInt(num.toString().split("").reverse().join(""), 10);
@@ -51,3 +55,63 @@ console.log(currentDate["mm-dd-yyyy"]);
 console.log(currentDate["mm/dd/yyyy"]);
 console.log(currentDate["dd-mm-yyyy"]);
 console.log(currentDate["dd/mm/yyyy"]);
+
+// 5)
+function capitalize(str) {
+  if (!str) return str;
+
+  let firstChar = str.charAt(0);
+  let restOfString = str.slice(1);
+
+  if (firstChar >= "a" && firstChar <= "z") {
+    firstChar = firstChar.toUpperCase();
+  }
+
+  return firstChar + restOfString;
+}
+
+console.log(capitalize("noman"));
+console.log(capitalize("is"));
+console.log(capitalize("The"));
+console.log(capitalize("coolest"));
+
+// 6)
+function checkPeriod(str) {
+  if (str.includes(".")) {
+    return "contains period";
+  } else {
+    return "no period";
+  }
+}
+
+console.log(checkPeriod("hello there"));
+console.log(checkPeriod("..."));
+console.log(checkPeriod("general"));
+console.log(checkPeriod("kenobi"));
+
+// 7)
+function putSuffix(num) {
+  if (num === undefined) return;
+
+  let suffix = "th";
+  let lastDigit = num % 10;
+  let lastTwoDigits = num % 100;
+
+  if (lastTwoDigits < 11 || lastTwoDigits > 13) {
+    if (lastDigit === 1) {
+      suffix = "st";
+    } else if (lastDigit === 2) {
+      suffix = "nd";
+    } else if (lastDigit === 3) {
+      suffix = "rd";
+    }
+  }
+
+  return num + suffix;
+}
+
+console.log(putSuffix(22));
+console.log(putSuffix(23));
+console.log(putSuffix(1));
+console.log(putSuffix(2));
+console.log(putSuffix());
